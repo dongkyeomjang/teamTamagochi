59125893768b89c1675d30ac525f67b0585055d9
dongkyeomjang <jangelliot0404@dgu.ac.kr>
2024-01-07 03:53:01 +0900

    feat: 게이지바 객체 추가

---
+++ src/SatietyBar.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class SatietyBar extends Drawable{
+	public SatietyBar(int x, int y, int size, String imgURL) {
+		super(x,y,size,imgURL);
+	}
+	public void display(Graphics g) {
+		Image img = Toolkit.getDefaultToolkit().getImage(getImgURL());
+        g.drawImage(img, getX(), getY(), getSize(), getSize(), null);
+	}
+}
