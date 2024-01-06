3fc64397905cd620dd98f8459c0a3a8a589a54cb
ChoiHyebin <91861082+ChoiHyebin@users.noreply.github.com>
2024-01-06 20:00:23 +0900

    Update TamaManager.java
    ✨ 다마고치의 캐릭터가 사망했을 때 타이머를 멈추게 하기 위해 feed() 메소드와 gettingHungry() 메소드가 hungryTimer와 sleepyTimer를 매개변수로 받아 타이머를 멈추게 함. 캐릭터가 사망했을 때 사망 알림창이 뜸.

--- src/TamaManager.java
+++ src/TamaManager.java
@@ -1,30 +1,9 @@
-//import java.util.ArrayList;
-//import java.util.Random;
-//
-//public class TamaManager {
-//	private Tamagochi tama;
-//	private ArrayList<Tombstone> tombstones;
-//	private ArrayList<Poop> poops;
-//	
-//	public TamaManager() {
-//		
-//	}
-//	
-//	public void feed() {
-//		Random rand = new Random();
-//		
-//		tama.setSatiety(tama.getSatiety() + rand.nextInt(3) + 1); // rand.nextInt(3) => 0~2, 밥 먹이면 포만감 1~3 랜덤으로 차야 됨
-//	}
-//	
-//	public void gettingHungry() {
-//		tama.setSatiety(tama.getSatiety()-1); // 시간이 10초 흐를 때마다 포만감이 1씩 줄어듦
-//	}
-//	
-//}
-
 
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
 
 public class TamaManager {
     private Tamagochi tama;
@@ -32,45 +11,52 @@ public class TamaManager {
     private ArrayList<Tombstone> tombstones;
     
     Random rand = new Random();
-    private int food;
-    private int state;
     
 	public TamaManager(){
         this.tama = null;
         poops = new ArrayList<Poop>();
         tombstones = new ArrayList<Tombstone>();
-        this.state = 1; // 0 되면 죽은 거
     }
     public void createTama(String nickname){
         int x=50, y=50, size=50;
         tama = new Tamagochi(x, y, size, "src/img/tamagochiImg.png", nickname);
     }
     
-    public void repaintTama() {
-    	int x = 50, y = 50, size = 50;
-    	tama = new Tamagochi(x, y, size, "src/img/tama_ghost.png", tama.getNickname());
-    }
-    
-    public void feed(){
+    public void feed(Timer hungryTimer, Timer sleepyTimer){
         Random random= new Random();
-        food = rand.nextInt(3) + 1;
+        
+        String[] buttons = {"종료", "재실행"};
         
         //밥 먹임. 랜덤하게 1~3만큼 포만감 증가
-        tama.setSatiety(tama.getSatiety()+food);
+        tama.setSatiety(tama.getSatiety()+random.nextInt(3)+1);
         
         // 포만감이 11 이상인 경우, 일정 확률로 Tamagochi의 dieByEating() 메소드 호출 => 배부른 상태에서 밥 먹이면 50% 확률로 죽음
         if(tama.getSatiety() >= 11) {
         	if(Math.random() < 0.5) {
-        		state = 0;
                 tombstones.add(tama.dieByEating("죽을때까지 먹다가 배터져 죽음"));
                 System.out.println("죽음");
+                hungryTimer.stop();
+                sleepyTimer.stop();
+                
+            	int result = JOptionPane.showOptionDialog(null, "사망했습니다.", "사망 알림창", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, buttons, "재실행");
+            	System.out.println(result);
+            	if(result == 0) {
+            		System.exit(1);
+            	}
         	}
         }
         // 만약 포만감이 15를 초과했을 경우, Tamagochi의 dieByEating() 메소드 호출
         if(tama.getSatiety() >= 15){
-    		state = 0;
             tombstones.add(tama.dieByEating("죽을때까지 먹다가 배터져 죽음"));
             System.out.println("죽음");
+            hungryTimer.stop();
+            sleepyTimer.stop();
+            
+        	int result = JOptionPane.showOptionDialog(null, "사망했습니다.", "사망 알림창", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, buttons, "재실행");
+        	System.out.println(result);
+        	if(result == 0) {
+        		System.exit(1);
+        	}
         }
     }
     
@@ -84,40 +70,48 @@ public class TamaManager {
     }
     public void createPoop(){
         // poop 생성
-        Random random = new Random();
-        // 화면 크기에 맞추어 랜덤 좌표 생성 (예시: 화면 크기가 500x500)
-        int x = random.nextInt(450); // 화면 너비에서 poop 크기(50)를 뺀 범위
-        int y = random.nextInt(450); // 화면 높이에서 poop 크기(50)를 뺀 범위
-        
-        int size=50;
+        int x=50, y=50, size=50;
         Poop poop = new Poop(x, y, size, "src/img/poop.png");
         poops.add(poop);
         // 만약 poop가 10개라면, Tamagochi의 dieByPoop() 메소드 호출. 즉 poop이 10개가 되면 죽음.
         if(poops.size() >= 10){
             tombstones.add(tama.dieByPoop("똥독 올라 죽음"));
         }
-        System.out.println("똥생성");
-        System.out.println(x);
-        System.out.println(y);
     }
-    public void gettingHungry(){
+    public void gettingHungry(Timer hungryTimer, Timer sleepyTimer){
         // 시스템은 tama의 createTime과 현재 시간을 비교하여 시간의 경과를 측정하고, '10초'마다 포만감을 1씩 감소시킴
         tama.setSatiety(tama.getSatiety()-1);
 
+        String[] buttons = {"종료", "재실행"};
+        
         // 포만감이 4 이하인 경우 50% 확률로 Tamagochi의 dieByEating() 메소드 호출하여 죽게 함
         if(tama.getSatiety() <= 4) {
         	if(Math.random() < 0.5) {
-        		state = 0;
                 tombstones.add(tama.dieByEating("굶어 죽음"));
                 System.out.println("죽음");
+                hungryTimer.stop();
+                sleepyTimer.stop();
+                
+            	int result = JOptionPane.showOptionDialog(null, "사망했습니다.", "사망 알림창", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, buttons, "재실행");
+            	System.out.println(result);
+            	if(result == 0) {
+            		System.exit(1);
+            	}
         	}
         }
         
         // 포만감이 0일 때는 Tamagochi의 dieByEating() 메소드를 호출이여 무조건 죽게 함
         if(tama.getSatiety() == 0) {
-    		state = 0;
             tombstones.add(tama.dieByEating("굶어 죽음"));
             System.out.println("아사");
+            hungryTimer.stop();
+            sleepyTimer.stop();
+
+        	int result = JOptionPane.showOptionDialog(null, "사망했습니다.", "사망 알림창", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, buttons, "재실행");
+        	System.out.println(result);
+        	if(result == 0) {
+        		System.exit(1);
+        	}
         }
     }
     public void gettingSleepy() {
@@ -136,18 +130,5 @@ public class TamaManager {
     public Tamagochi getTama(){
         return tama;
     }
-
-    public int getFood() {
-		return food;
-	}
-	public void setFood(int food) {
-		this.food = food;
-	}
     
-	public int getState() {
-		return state;
-	}
-	public void setState(int state) {
-		this.state = state;
-	}
 }
