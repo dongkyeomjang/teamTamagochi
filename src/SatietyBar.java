3ca445a83653d13143704ec8a87a1b49b27aa8ac
장동겸 <86873281+dongkyeomjang@users.noreply.github.com>
2024-01-07 10:26:43 +0900

    migrate intelliJ->eclipse

--- src/SatietyBar.java
+++ src/SatietyBar.java
@@ -1,12 +1,11 @@
 import java.awt.*;
 
-
 public class SatietyBar extends Drawable {
     public SatietyBar(int x, int y, int size, String imgURL) {
         super(x,y,size,imgURL);
     }
     public void display(Graphics g) {
         Image img = this.imgIcon.getImage();
-        g.drawImage(img, getX(), getY(), getSize(), getSize(), null);
+        g.drawImage(img, getX(), getY(), 125, 20, null);
     }
 }
