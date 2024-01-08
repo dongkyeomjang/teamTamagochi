a202e6e14dd51bf329fb10c8e285dcd66f46e1e0
SemiHyeon <gustpal08@gmai.com>
2024-01-08 19:59:36 +0900

    fix: x로 강제 종료시 종료

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         MyFrame myframe = new MyFrame();
+        myframe.setDefaultCloseOperation(MyFrame.EXIT_ON_CLOSE);
     }
 }
